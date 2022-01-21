69ec97ccffcb25e96eb6f2ba90da16f660c9549e
Everson Ott <everson.ott@gmail.com>
2022-01-21 15:21:53 -0300

    Uso de Templates - Part 1

--- live_56_desenvolvimento_web_com_python_e_flask/views.py
+++ live_56_desenvolvimento_web_com_python_e_flask/views.py
@@ -1,4 +1,4 @@
-from flask import jsonify
+from flask import jsonify, render_template
 
 def configure(app):
     @app.route("/")
@@ -7,4 +7,12 @@ def configure(app):
     @app.route("/api")
     def api():
         return jsonify(data = {'nome': 'Dimas'})
+    @app.route("/langs")
+    def langs():
+        ling = ['Python', 'Bash', 'C++', 'Java']
+        return render_template(
+            'index.html',
+            titulo = "Melhores Linguagens",
+            linguagens = ling
+        )
 

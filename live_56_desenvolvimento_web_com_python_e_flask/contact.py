1ab259296a0f27955f2e3090c291533864e99fc6
Everson Ott <everson.ott@gmail.com>
2022-01-21 16:54:49 -0300

    Uso de templates - Part 2, com métodos POST e GET

--- live_56_desenvolvimento_web_com_python_e_flask/contact.py
+++ live_56_desenvolvimento_web_com_python_e_flask/contact.py
@@ -1,10 +1,18 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request
+
 
 bp = Blueprint('contact', __name__, url_prefix = '/contact')
 
-@bp.route('/')
+@bp.route('/', methods=['GET','POST'])
 def contact():
-    return "Fale conosco"
+    if request.method == 'GET':
+        return render_template('contact.html')
+
+    print(request.form)
+    nome = request.form.get('nome')
+    mensagem = request.form.get('mensagem')
+    print(nome)
+    return f"Olá {nome}. Sua mensagem foi enviada"
 
 def configure(app):
     app.register_blueprint(bp)
